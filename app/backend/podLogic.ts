7e89d1f0f1e0af3af75036d0111da63427ab8929
snepvangerstwan-oss <snepvangerstwan@gmail.com>
2026-04-14 18:21:42 +0200

    Change session end time calculation
    Updated session duration from 60 seconds to 120 seconds.

--- app/backend/podLogic.ts
+++ app/backend/podLogic.ts
@@ -69,7 +69,7 @@ export function startSession(
     pod.sessionStartedBy = startedBy;
 
     pod.SessionStart = new Date();
-    pod.SessionEnd = new Date(pod.SessionStart.getTime() +  60 * 1000); // 2 hours now 60 seconds
+    pod.SessionEnd = new Date(pod.SessionStart.getTime() +  120 * 1000); // 2 hours now 120 seconds
     
     pod.ReservationExpiry = null;
     pod.motionStart = null;
